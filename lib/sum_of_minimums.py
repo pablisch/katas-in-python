c95bdd2d8c1e092910b87928e0102edb08562327
Pablo <pablito@me.com>
2024-08-31 14:24:01 +0100

    feat: sum of minimums.

--- lib/sum_of_minimums.py
+++ lib/sum_of_minimums.py
@@ -1,7 +1,13 @@
-#
-# KEYWORDS: nested_list, sum,
+# https://www.codewars.com/kata/5d5ee4c35162d9001af7d699/solutions/python
+# KEYWORDS: nested_list, sum, minimum, for, map
 
 def sum_of_minimums(lst):
-    mins = 3
+    return sum(min(arr) for arr in lst)
 
-print (min(1,4))
+# Adjusting to Python syntax, map takes in a function and an iterable, so here, .min() applies to each el of numbers.
+def sum_of_minimums_cw(numbers):
+    return sum(map(min, numbers))
+
+list_ = [[1,4],[2,6], [6,8]]
+
+sum_of_minimums(list_)
--- tests/test_sum_of_minimums.py
+++ tests/test_sum_of_minimums.py
@@ -2,8 +2,15 @@ import pytest
 from lib.sum_of_minimums import *
 
 @pytest.mark.parametrize('lst, expected_result', [
-    ([], 0),
+    ([[1,4],[2,6], [6,8]], 9),
 ])
 def test_sum_of_minimums_returns_the_sum_of_minimum_values(lst, expected_result):
     result = sum_of_minimums(lst)
+    assert result == expected_result
+
+@pytest.mark.parametrize('lst, expected_result', [
+    ([[1,4],[2,6], [6,8]], 9),
+])
+def test_sum_of_minimums_cw_returns_the_sum_of_minimum_values(lst, expected_result):
+    result = sum_of_minimums_cw(lst)
     assert result == expected_result
